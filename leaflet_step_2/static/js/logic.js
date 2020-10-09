dbb847f0ce5344647f360b3252d02355fbe51736
BklynIrish <bm624@nyu.edu>
2020-10-09 15:18:07 -0400

    updated files

--- leaflet_step_2/static/js/logic.js
+++ leaflet_step_2/static/js/logic.js
@@ -1,11 +1,11 @@
-// Store API endpoint inside queryUrl
+// Store our API endpoint inside queryUrl
 var queryUrl = ["https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson",
     "./static/GeoJSON/PB2002_boundaries.json",
     "./static/GeoJSON/PB2002_orogens.json",
     "./static/GeoJSON/PB2002_plates.json"
 ]
 
-// Define variables for base layers
+// Define variables for our base layers
 var streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     tileSize: 512,
@@ -33,8 +33,7 @@ var satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/
     accessToken: API_KEY
 })
 
-// make the streetmap and earthquakes' layers
-to display on load
+// Create our map, giving it the streetmap and earthquakes layers to display on load
 var myMap = L.map("map", {
     center: [
         37.09, -95.71
@@ -50,7 +49,7 @@ let baseMaps = {
     "Satellite": satelliteStreets
 };
 
-// Define the layers.
+// Create the layers for our two different sets of data, earthquakes and tectonicplates.
 let earthquakesLayer = new L.LayerGroup();
 let boundaryLayer = new L.LayerGroup();
 let orogensLayer = new L.LayerGroup();
@@ -58,8 +57,8 @@ let platesLayer = new L.LayerGroup();
 
 // let tectonicplates = new L.LayerGroup();
 
-// define an object containing all the overlays.
-// we hope it pops up as an option
+// We define an object contains all of our overlays.
+// This overlay will be visible all the time.
 let overlayMaps = {
 
     "Earthquakes": earthquakesLayer,
@@ -68,7 +67,7 @@ let overlayMaps = {
     "Plates": platesLayer
 };
 
-// now the mechanism by which you switch layers and allow to then see which
+// Then we add a control to the map that will allow the user to change which
 // layers are visible.
 L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
